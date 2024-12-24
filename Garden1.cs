84e820e555b413917904ee48cac7ee0abaa64970
Mukhamed005 <156431717+Mukhamed005@users.noreply.github.com>
2024-12-24 10:54:37 +0300

    Add files via upload

---
+++ Garden1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GardenLibrary
+{
+    public class Garden
+    {
+        public Address Address { get; set; }
+        public string Owner { get; set; }
+        public List<Flower> Flowers { get; set; }
+
+        public Garden()
+        {
+            Flowers = new List<Flower>();
+        }
+        public void AddFlower(Flower flower)
+        {
+            Flowers.Add(flower);
+        }
+        public void ShowAll()
+        {
+            foreach (var flower in Flowers)
+            {
+                flower.show();
+            }
+        }
+    }
+
+
+    public class Flower
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+
+        public void show()
+        {
+            Console.WriteLine($"title:{title},description{description}");
+        }
+    }
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Zipcode { get; set; }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using GardenLibrary;
+
+Garden garden = new Garden
+{  Address = new Address { Street = "Староватутинский", City = "Москва", Zipcode = "123" }, Owner = "Адам" };
+garden.AddFlower(new Flower { title = "sgdse", description = "fbgdfre" });
+Console.WriteLine("Flowers 8in the gaden");
+garden.ShowAll();
+Console.ReadKey();
